c54f8ac9be371e48cdeb74606a4ec3904ffd8376
“PABLO” <pablomanuel93@hotmail.com>
2024-05-21 13:08:14 +0200

    correcion error de sintaxis en description

--- src/database/migrations/20240521101852-create-services.js
+++ src/database/migrations/20240521101852-create-services.js
@@ -14,7 +14,7 @@ module.exports = {
         allowNull:false,
         unique:true,
       },
-      descripttion:{
+      description:{
         type:Sequelize.TEXT
       },
       createdAt: {
--- src/database/seeders/20240521103953-seed-roles.js
+++ src/database/seeders/20240521103953-seed-roles.js
@@ -3,23 +3,35 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert(
+      "roles",
+      [
+        {
+          id: 1,
+          name: "user",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 2,
+          name: "admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: 3,
+          name: "super-admin",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+   
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete("roles", null, {});
+  
   }
 };
